a69c46abb4bde17682be4dd6d9656c07621596f5
ledenis <ledenis.git@gmail.com>
2014-02-20 18:35:27 +0000

    Add "split the bill" dialog with NumberPicker

--- SmartBill/src/com/me/smartbill/activity/MainActivity.java
+++ SmartBill/src/com/me/smartbill/activity/MainActivity.java
@@ -1,23 +1,34 @@
 package com.me.smartbill.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.View;
 
 import com.me.smartbill.R;
 
-public class MainActivity extends PreferenceActivity {
+public class MainActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		// Include the preference screen
 		addPreferencesFromResource(R.xml.prefs);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		PreferenceManager.getDefaultSharedPreferences(this)
+				.registerOnSharedPreferenceChangeListener(this);
 	}
 
 	@Override
@@ -25,11 +36,17 @@ public class MainActivity extends PreferenceActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	public void calculateClick(View view) {
 		Intent intent = new Intent(this, ResultActivity.class);
-		
+
 		startActivity(intent);
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		
 	}
 
 }
--- SmartBill/src/com/me/smartbill/preference/SplitPreference.java
+++ SmartBill/src/com/me/smartbill/preference/SplitPreference.java
@@ -1,13 +1,44 @@
 package com.me.smartbill.preference;
 
+import com.me.smartbill.R;
+
 import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.NumberPicker;
 
 public class SplitPreference extends DialogPreference {
 	
+	public static final int DEFAULT = 1;
+	private NumberPicker nbPicker;
+
 	public SplitPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		
+		setDialogLayoutResource(R.layout.split_pref_layout);
+	}
+
+	@Override
+	protected void onBindDialogView(View view) {
+		super.onBindDialogView(view);
+		
+		nbPicker = (NumberPicker) view.findViewById(R.id.nbPicker);
+		int persistedInt = getPersistedInt(DEFAULT);
+		
+		nbPicker.setMinValue(1);
+		nbPicker.setMaxValue(1000);
+		nbPicker.setValue(persistedInt);
+		nbPicker.setWrapSelectorWheel(false);
+	}
+
+	@Override
+	protected void onDialogClosed(boolean positive) {
+		super.onDialogClosed(positive);
+		
+		if (positive) {
+			persistInt(nbPicker.getValue());
+		}
 	}
 	
 }
