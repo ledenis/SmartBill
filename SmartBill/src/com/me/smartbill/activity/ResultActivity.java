43517b81fb7c20088166a5bca0d83547eb3a2536
ledenis <ledenis.git@gmail.com>
2014-02-20 17:33:06 +0000

    Various UI changes

--- SmartBill/src/com/me/smartbill/activity/ResultActivity.java
+++ SmartBill/src/com/me/smartbill/activity/ResultActivity.java
@@ -1,7 +1,17 @@
 package com.me.smartbill.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.me.smartbill.R;
 
 public class ResultActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.result_layout);
+	}
 
 }
