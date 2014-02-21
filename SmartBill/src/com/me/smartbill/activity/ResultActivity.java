e1054ba743e7adbbb86df97eec6991914d12f453
ledenis <ledenis.git@gmail.com>
2014-02-21 16:33:15 +0000

    Retrieve input to ResultActivity

--- SmartBill/src/com/me/smartbill/activity/ResultActivity.java
+++ SmartBill/src/com/me/smartbill/activity/ResultActivity.java
@@ -1,17 +1,40 @@
 package com.me.smartbill.activity;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.me.smartbill.R;
+import com.me.smartbill.preference.SplitPreference;
+import com.me.smartbill.preference.TipPreference;
 
 public class ResultActivity extends Activity {
+	// XXX
+	// private float bill;
+	private boolean isCash;
+	private float tip;
+	private int split;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.result_layout);
+
+		retrieveInput();
+
+		System.out.println("isCash : " + isCash);
+		System.out.println("tip : " + tip);
+		System.out.println("split : " + split);
 	}
 
+	private void retrieveInput() {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+
+		isCash = prefs.getBoolean("cash", false);
+		tip = prefs.getInt("tip", TipPreference.DEFAULT) / 100f;
+		split = prefs.getInt("split", SplitPreference.DEFAULT);
+	}
 }
