e03f2e76b11be3206ea3929e7c3be9f6fe29a7db
ledenis <ledenis.git@gmail.com>
2014-02-21 16:06:55 +0000

    Update preference summaries

--- SmartBill/src/com/me/smartbill/activity/MainActivity.java
+++ SmartBill/src/com/me/smartbill/activity/MainActivity.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.View;
 
 import com.me.smartbill.R;
+import com.me.smartbill.preference.SplitPreference;
+import com.me.smartbill.preference.TipPreference;
 
-public class MainActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+public class MainActivity extends PreferenceActivity implements
+		OnSharedPreferenceChangeListener {
 
 	@SuppressWarnings("deprecation")
+	// compatibility with older API
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,6 +27,12 @@ public class MainActivity extends PreferenceActivity implements OnSharedPreferen
 
 		// Include the preference screen
 		addPreferencesFromResource(R.xml.prefs);
+
+		// Init preference summaries
+		for (String key : PreferenceManager.getDefaultSharedPreferences(this)
+				.getAll().keySet()) {
+			updateSummary(key);
+		}
 	}
 
 	@Override
@@ -44,9 +56,29 @@ public class MainActivity extends PreferenceActivity implements OnSharedPreferen
 	}
 
 	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+	public void onSharedPreferenceChanged(SharedPreferences preferences,
 			String key) {
-		
+		@SuppressWarnings("deprecation")
+		// compatibility with older API
+		Preference pref = findPreference(key);
+
+		updateSummary(pref.getKey());
+	}
+
+	@SuppressWarnings("deprecation")
+	private void updateSummary(String key) {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+
+		if (key.equals("tip")) {
+			System.out.println("tipprf");
+			TipPreference p = (TipPreference) findPreference(key);
+			p.setSummary("" + prefs.getInt(key, TipPreference.DEFAULT) + " %");
+		} else if (key.equals("split")) {
+			SplitPreference p = (SplitPreference) findPreference(key);
+			p.setSummary("" + prefs.getInt(key, SplitPreference.DEFAULT) + " "
+					+ getString(R.string.persons));
+		}
 	}
 
 }
